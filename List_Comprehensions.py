ff39bc23b3e30bd7da46c137dfcbbe134689315d
Shreesha <hegde.shreesha2007@gmail.com>
2026-06-10 21:40:03 +0530

    enhance List_Comprehensions.py with user prompts for cuboid dimensions

--- List_Comprehensions.py
+++ List_Comprehensions.py
@@ -1,7 +1,11 @@
-x = int(input())
-y = int(input())
-z = int(input())
-n = int(input())
+# You are given three integers x,y and z representing the dimensions of a cuboid along with an integer n.
+# Print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum of i+j+k is not equal to n.
+# Here, 0<=i<=x; 0<=j<=y; 0<=k<=z;
+
+x = int(input("Enter first dimension of cuboid"))
+y = int(input("Enter second dimension of cuboid"))
+z = int(input("Enter third dimension of cuboid"))
+n = int(input("Enter the number n"))
 result=[]
 for i in range(x + 1):
     for j in range(y + 1):
