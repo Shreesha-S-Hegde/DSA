5e17ce543d9e55d01cfd076331a5395b175df1ff
Shreesha <hegde.shreesha2007@gmail.com>
2026-06-26 16:04:53 +0530

    Add implementation to increment a list of digits by one

---
+++ 19_Plus_one.py
@@ -0,0 +1,12 @@
+digits = [1,2,3]
+n=len(digits)
+l=[]
+i=0
+new = ' '
+for item in digits:
+    new+=str(item)
+new=int(new)
+new+=1
+for items in str(new):
+    l.append(int(items))
+print("The appended list is",l)
