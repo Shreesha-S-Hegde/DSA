1b21b7a0b52b0544ffa83e61748ec018fe03b9de
Shreesha <hegde.shreesha2007@gmail.com>
2026-07-09 23:40:16 +0530

    Remove implementation of finding the single number in a list

--- 100_Single_Number.py
+++
@@ -1,9 +0,0 @@
-nums = [4,1,2,1,2]
-n=len(nums)
-for i in range(n):
-    count=0
-    for j in range(n):
-        if nums[i]==nums[j]:
-            count+=1
-    if count==1:
-        print(nums[i])
